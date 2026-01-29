6a1c0da76d9c58552182b5c6ddcd75377025309f
yurzzilla <bu277188@gmail.com>
2026-01-29 09:46:31 +0200

    1.2 Minor Aesthetics changes and little code improvements

--- Snake_1.0/Snake_1.0.ino
+++ Snake_1.0/Snake_1.0.ino
@@ -5,14 +5,10 @@ signed int SnakeX[64];
 signed int SnakeY[64];
 int length = 3;
 int direction = 0;
-int r;
-int r2;
-int xa = random(0,7);
-int ya = random(0,7);
+int xa, ya, r, r2;
 int game = 1;
 
 void move(){
-  
   if(SnakeX[0] > 7) SnakeX[0] = 0;
   if(SnakeY[0] > 7) SnakeY[0] = 0;
   if(SnakeX[0] < 0) SnakeX[0] = 7;
@@ -23,6 +19,7 @@ void move(){
     SnakeX[i] = SnakeX[i-1];
     SnakeY[i] = SnakeY[i-1];
   }
+
   if(direction == 0){
     SnakeY[0] += 1;
   }if(direction == 1){
@@ -33,14 +30,10 @@ void move(){
     SnakeX[0] -= 1;
   }
   
-
-
-  
   lc.clearDisplay(0);
   for(int i = 0; i < length; i++){
     lc.setLed(0, SnakeX[i], SnakeY[i], true);
   }
-
   for(int i = length-1; i>0; i--){
     if(SnakeX[0] == SnakeX[i] && SnakeY[0] == SnakeY[i]){
       game = 0;
@@ -51,7 +44,9 @@ void move(){
 }
 
 void setup() {
-  Serial.begin(9600);
+  randomSeed(analogRead(0));
+  xa = random(0,7);
+  ya = random(0,7);
   pinMode(A4, INPUT);
   pinMode(A5, INPUT);
   lc.shutdown(0, false);   
@@ -65,7 +60,9 @@ void setup() {
   SnakeX[2] = 0;
   SnakeY[2] = 3;
 }
+
 void loop() {
+  randomSeed(analogRead(0));
   if(game){ 
     lc.setLed(0,xa,ya, true);
     for(int i = length-1; i>0; i--){
@@ -86,16 +83,18 @@ void loop() {
     else if(r >= 600 && direction != 0) direction = 1;
     if(r2 <= 400 && direction != 3) direction = 2;
     else if(r2 >= 600 && direction != 2) direction = 3;
-    Serial.println(direction);
     move();
     lc.setLed(0,xa,ya, true);
     delay(300);
   }
   else{
-    for(int i = length-1; i>-1; i--){
-      lc.setLed(0, SnakeX[i], SnakeY[i], true);
-      delay(100);
+    for(int i = 0; i < length; i++){
       lc.setLed(0, SnakeX[i], SnakeY[i], false);
     }
+    delay(200);
+    for(int i = 0; i<length; i++){
+      lc.setLed(0, SnakeX[i], SnakeY[i], true);
+    }
+    delay(200);
   }
 }
